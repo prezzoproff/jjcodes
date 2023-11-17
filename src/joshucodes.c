1260d04f0d3d581fdbf94633a1ebc1ae241e29fe
Joshucodes <ndilijoshua28@gmail.com>
2023-11-17 03:40:25 -0800

    added egine

---
+++ src/joshucodes.c
@@ -0,0 +1,9 @@
+// progam that prints joshua was position 3 with grade A
+#include<stdio.h>
+int main()
+{
+    int position=2; // var declaration and init
+    char grade = 'A';
+    printf("joshua was position %d with grade %c this term",position,grade ); // statement
+    return 0;
+}
