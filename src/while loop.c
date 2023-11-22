330df1e0a099d2e974249f1bab19486745ebe543
Joshucodes <ndilijoshua28@gmail.com>
2023-11-22 03:35:51 -0800

    while loop

---
+++ src/while loop.c
@@ -0,0 +1,14 @@
+// program to print numbers from 1-10
+
+#include <stdio.h>
+int main ()
+{
+    int n=101;             // 1. var declaration and initialisation
+    while (n>1)           //2. condition
+    {
+        printf("%d\n",n);     // Statements
+        n--;                   
+    }
+    return 0;
+}
+
