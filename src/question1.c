93d0d80b675577a3aa1b881ab1c3d7b136d3fc3b
Joshucodes <ndilijoshua28@gmail.com>
2023-11-22 04:22:04 -0800

    question1

---
+++ src/question1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main ()
+{
+    // int p=1;
+    // while(p<101)
+    // {
+    //     if(p%2!=0)
+    //     {
+    //         printf("%d\n", p);
+    //     }
+    //     p++;
+
+    // }
+    for(int p=1;p<101;p++)
+    {
+        if(p%2!=0)
+        {
+            printf("%d\n", p);
+        }
+    }
+}
