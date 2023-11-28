7f157d5393419529834009f7883130706c6f7e44
Joshucodes <ndilijoshua28@gmail.com>
2023-11-27 21:42:12 -0800

    grading system

---
+++ src/dddddddd.c
@@ -0,0 +1,4 @@
+// program that prompts the user to enter two numbers and muultiplies them
+#include <stdio.h>
+int main ()
+}
---
+++ src/divisionfucntion.c
@@ -0,0 +1,20 @@
+// program that uses a function to divide two numbers
+
+#include <stdio.h>
+
+int divider (int a, int b);
+int main ()
+{
+
+    int division=divider(a,b);
+    scanf("%d", division);{
+
+        int divider ( int a, int b){
+            int divide;
+            divide=a/b;
+            return divide;
+
+        }
+    }
+}
+
