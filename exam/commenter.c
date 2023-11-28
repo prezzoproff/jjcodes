455a2b41aae92b9da555f0535e99657e22ca7dd5
prezzoproff <john1thelegend@gmail.com>
2023-11-28 11:44:37 +0300

    commenter function

---
+++ exam/commenter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int commenter(char grade)
+{
+    if (grade == 'A')
+    {
+        printf("Excellent");
+    }else if (grade == 'B')
+    {
+        printf("Very good");
+    }else if (grade == 'C')
+    {
+        printf("Good Trial");
+    }else
+    {
+        printf("Rudi kwenu ukalale bana");
+    }
+       
+}
